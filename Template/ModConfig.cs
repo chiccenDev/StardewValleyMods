27de715fb89d16964689280dc90c678cb2d5f7ec
Joshua Parks <joshuaparks2018@gmail.com>
2024-05-20 12:11:47 -0400

    init mods

---
+++ Template/ModConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace MyMod
+{
+    public class ModConfig
+    {
+
+        public bool ExampleBool { get; set; } = true;
+        public int ExampleInt { get; set; } = 16;
+        public bool Debug { get; set; } = false;
+
+    }
+}
---
+++ WaterYourCrops/ModConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace WaterYourCrops
+{
+    public class ModConfig
+    {
+
+        public bool ExampleBool { get; set; } = true;
+        public int ExampleInt { get; set; } = 16;
+        public bool Debug { get; set; } = false;
+
+    }
+}
