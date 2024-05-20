eecec63a985406a55a1526f44b217f0fc25d9192
Joshua Parks <joshuaparks2018@gmail.com>
2024-05-20 18:35:50 -0400

    config localization

--- WaterYourCrops/ModConfig.cs
+++ WaterYourCrops/ModConfig.cs
@@ -1,10 +1,12 @@
-﻿namespace WaterYourCrops
+﻿using Microsoft.Xna.Framework;
+
+namespace WaterYourCrops
 {
     public class ModConfig
     {
         public bool EnableMod { get; set; } = true;
         public bool OnlyWaterCan { get; set; } = true;
-        public string IndicatorColor { get; set; } = "White";
+        public Color IndicatorColor { get; set; } = Color.White;
         public float IndicatorOpacity { get; set; } = 1f;
         public bool Debug { get; set; } = false;
 
