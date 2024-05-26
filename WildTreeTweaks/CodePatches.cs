eee6a440c226af6fa0ffc14f1ca94c8ad3f039b4
Joshua Parks <joshuaparks2018@gmail.com>
2024-05-26 15:14:08 -0400

    patch IsGrowthBlockedByNearbyTree for GrowNearTrees

--- WildTreeTweaks/CodePatches.cs
+++ WildTreeTweaks/CodePatches.cs
@@ -70,10 +70,10 @@ namespace WildTreeTweaks
         [HarmonyPatch(typeof(Tree), nameof(Tree.IsGrowthBlockedByNearbyTree))]
         public class Tree_IsGrowthBlockedByNearbyTree_Patch
         {
-            public static bool Prefix(Tree __instance, ref bool __result)
+            public static void Postfix(Tree __instance, ref bool __result)
             {
-                __result = (Config.EnableMod && Config.GrowNearTrees);
-                return !__result;
+                __result = !(Config.EnableMod && Config.GrowNearTrees);
+                return;
             }
         }
 
@@ -144,11 +144,11 @@ namespace WildTreeTweaks
                 if (!(Tree.GetWildTreeDataDictionary().TryGetValue(id, out data))) return true;
 
                 data.GrowthChance = Config.GrowthChance; // 1f = always true, called in Tree.dayUpdate()
-                data.GrowsInWinter = Config.GrowInWinter; // called in dayUpdate()
+                data.GrowsInWinter = Config.GrowInWinter; // called in IsInSeason() => GetMaxSizeHere() => dayUpdate()
                 data.IsStumpDuringWinter = !Config.GrowInWinter; // called in dayUpdate()
-                data.SeedOnShakeChance = Config.SeedChance; // 1f = always true, called in Tree.dayUpdate()
                 data.SeedSpreadChance = Config.SeedSpreadChance; // 1f = always true, called in Tree.dayUpdate()
                 float difChance = (data.SeedOnShakeChance - Config.SeedChance) * 10f; // seed chop scales with seed shake. lowest possible val for seed chop = 0.25 = 25% chance
+                data.SeedOnShakeChance = Config.SeedChance; // 1f = always true, called in Tree.dayUpdate()
                 data.SeedOnChopChance = (difChance + data.SeedOnChopChance) > 1f ? 1f : data.SeedOnChopChance + difChance;
 
                 List<WildTreeChopItemData> chopItems = data.ChopItems;
