29b48d1ff9d842ec4ef013681dfb26ed26c364d0
Joshua Parks <joshuaparks2018@gmail.com>
2025-01-04 17:41:23 -0500

    postfix MapPage.receiveLeftClick

--- MapTeleport/CodePatches.cs
+++ MapTeleport/CodePatches.cs
@@ -1,20 +1,44 @@
-﻿using Microsoft.CodeAnalysis.FlowAnalysis;
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.FlowAnalysis;
 using StardewValley.Menus;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HarmonyLib;
 
 namespace MapTeleport
 {
     public partial class ModEntry
     {
-        public static bool CheckClickableComponents(List<ClickableComponent> components, int topX, int topY, int x, int y)
-        {
-            if (!Config.EnableMod) return false;
+        /* notes to myself before starting
+         * 
+         * GameStateQuery.CheckConditions(WorldMapTooltipData)
+         * 
+         * Content/Data/WorldMap.json is where you will find data about name/id, condition, knowncondition, etc
+         * 
+         * Game1.warpFarmer(string locationName, int tileX, int tileY, bool flip
+         * 
+        */
 
-            return true;
+        [HarmonyPatch(typeof(MapPage), nameof(MapPage.receiveLeftClick))]
+        public class MapPage_receiveLeftClick_Patch
+        {
+            public static void Postfix(MapPage __instance, int x, int y)
+            {
+                foreach (ClickableComponent c in __instance.points.Values)
+                {
+                    if (c.containsPoint(x, y))
+                    {
+                        if (Config.Simulate) TestWarp(c.name);
+                        else TryWarp(c.name);
+                    }
+                }
+
+                //return true;
+            }
         }
+
     }
 }
