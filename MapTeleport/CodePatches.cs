421f0ad10e39a53c2e6e68363bb1558b4f4395b5
Joshua Parks <joshuaparks2018@gmail.com>
2025-01-04 10:12:22 -0500

    mapteleport init

---
+++ MapTeleport/CodePatches.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.CodeAnalysis.FlowAnalysis;
+using StardewValley.Menus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapTeleport
+{
+    public partial class ModEntry
+    {
+        public static bool CheckClickableComponents(List<ClickableComponent> components, int topX, int topY, int x, int y)
+        {
+            if (!Config.EnableMod) return false;
+
+            return true;
+        }
+    }
+}
---
+++ MapTeleport/ModConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace MapTeleport
+{
+    public class ModConfig
+    {
+
+        public bool EnableMod { get; set; } = true;
+        public int ExampleInt { get; set; } = 16;
+        public bool Debug { get; set; } = false;
+
+    }
+}
