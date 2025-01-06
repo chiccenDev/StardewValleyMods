33b0be42b0a417a856a515abd4f0a7855127086f
Joshua Parks <joshuaparks2018@gmail.com>
2025-01-06 10:21:27 -0500

    cleanup

--- MapTeleport/CodePatches.cs
+++ MapTeleport/CodePatches.cs
@@ -1,33 +1,10 @@
-﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.FlowAnalysis;
-using StardewValley.Menus;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using StardewValley.Menus;
 using HarmonyLib;
 
 namespace MapTeleport
 {
     public partial class ModEntry
     {
-        /* notes to myself before starting
-         * 
-         * GameStateQuery.CheckConditions(WorldMapTooltipData)
-         * 
-         * Content/Data/WorldMap.json is where you will find data about name/id, condition, knowncondition, etc
-         * 
-         * Game1.warpFarmer(string locationName, int tileX, int tileY, bool flip
-         * 
-         * c.Tooltips is a List<WorldMapTooltipData> of shit including KnownCondition for GameStateQuery -- !!!
-         * 
-         * 
-         * more notes to myself for when i return from break
-         * 
-         * gamestatequery gonna b annoying because mappage.cs doesn't have all the same shiz as maparea.cs but its probably fine
-         * 
-        */
 
         [HarmonyPatch(typeof(MapPage), nameof(MapPage.receiveLeftClick))]
         public class MapPage_receiveLeftClick_Patch
