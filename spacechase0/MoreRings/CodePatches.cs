f654c1dab1eb90b653642bfc69dcebe5bcaed824
Joshua Parks <joshuaparks2018@gmail.com>
2025-05-16 17:18:51 -0500

    init spacechase0/MoreRings

---
+++ spacechase0/MoreRings/CodePatches.cs
@@ -0,0 +1,49 @@
+﻿using HarmonyLib;
+using StardewValley;
+using StardewValley.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreRings
+{
+    public partial class ModEntry
+    {
+
+        /// <summary>
+        /// Replacement for <see cref="ModEntry.OnItemEaten"/> to avoid limit prequisite APIs
+        /// </summary>
+        [HarmonyPatch(typeof(Farmer), nameof(Farmer.doneEating))]
+        public class Farmer_doneEating_Patch
+        {
+            public static void Postfix()
+            {
+                if (HasRingEquipped("Ring_of_Diamond_Booze"))
+                {
+                    if (Game1.player.hasBuff(Buff.tipsy))
+                    {
+                        Game1.player.buffs.Remove(Buff.tipsy);
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Replacement for AxePatcher.Before_DoFunction because I am just not familiar with that Harmony Patch formatting lol
+        /// </summary>
+        [HarmonyPatch(typeof(Axe), nameof(Axe.DoFunction))]
+        public class Axe_DoFunction_Patch
+        {
+            public static bool Prefix(ref int x, ref int y, Farmer who)
+            {
+                if (HasRingEquipped("Ring_of_Far_Reaching"))
+                {
+                    x = (int)who.lastClick.X;
+                    y = (int)who.lastClick.Y;
+                }
+                return true;
+            }
+        }
+    }
+}
---
+++ spacechase0/MoreRings/Methods.cs
@@ -0,0 +1,24 @@
+﻿using StardewValley;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreRings
+{
+    public partial class ModEntry
+    {
+        public static int CountRingsEquipped(string id)
+        {
+            int count = (Game1.player.leftRing.Value?.GetEffectsOfRingMultiplier(id) ?? 0) + (Game1.player.rightRing.Value?.GetEffectsOfRingMultiplier(id) ?? 0);
+
+            return count;
+        }
+
+        public static bool HasRingEquipped(string id)
+        {
+            return CountRingsEquipped(id) > 0;
+        }
+    }
+}
